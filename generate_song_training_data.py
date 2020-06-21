896cfeeb61a05f54bfb3ae9956ffd75eb0affd92
rearle <rb504035@dal.ca>
2020-06-21 15:51:44 -0300

    Trying to make our data generating script create larger samples

--- generate_song_training_data.py
+++ generate_song_training_data.py
@@ -28,67 +28,24 @@ def generate_graph_seq2seq_io_data(
 
     num_samples, num_nodes = midi_data.shape
     midi_data = np.expand_dims(midi_data, axis=-1)
-    # print(midi_data.shape)
-    # time.sleep(2)
-    # print("asdf" + 234)
-    #
-    # # num_samples, num_nodes = df.shape
-    # # data = np.expand_dims(df.values, axis=-1)
-    # # print(data.shape)
-    # # feature_list = [data]
-    # # if add_time_in_day:
-    # #     time_ind = (df.index.values - df.index.values.astype("datetime64[D]")) / np.timedelta64(1, "D")
-    # #     time_in_day = np.tile(time_ind, [1, num_nodes, 1]).transpose((2, 1, 0))
-    # #     feature_list.append(time_in_day)
-    # # if add_day_in_week:
-    # #     dow = df.index.dayofweek
-    # #     dow_tiled = np.tile(dow, [1, num_nodes, 1]).transpose((2, 1, 0))
-    # #     feature_list.append(dow_tiled)
-    #
-    # data = np.concatenate(feature_list, axis=-1)
-    # print(type(data))
-    # time.sleep(2)
-    # print("asdf"+234)
     x, y = [], []
     min_t = abs(min(x_offsets))
     max_t = abs(num_samples - abs(max(y_offsets)))  # Exclusive
+    print(min_t, max_t)
     for t in range(min_t, max_t):  # t is the index of the last observation.
         x.append(midi_data[t + x_offsets, ...])
         y.append(midi_data[t + y_offsets, ...])
-        # if t == 11:
-        #     print()
-        #     print(x_offsets)
-        #     print(y_offsets)
-        #     print(t)
-        #     print(t + x_offsets)
-        #     print(t + y_offsets)
-        #     print()
-        #     print(data.shape)
-        #     print(data[t + x_offsets, ...].shape)
-        #     print(data[t + y_offsets, ...].shape)
-        #     print(data[:12, 0, :] - data[t + x_offsets, 0, :])
-        #     print()
-        #     print(data[12:24, 0, :] - data[t + y_offsets, 0, :])
     x = np.stack(x, axis=0)
     y = np.stack(y, axis=0)
-    # print(x.shape)
-    # print(y.shape)
-    # time.sleep(2)
-    # print("asdf" + 234)
     return x, y
 
 
 def generate_train_val_test(args):
     seq_length_x, seq_length_y = args.seq_length_x, args.seq_length_y
-    # df = pd.read_hdf(args.traffic_df_filename)
     midi_data = np.transpose(pretty_midi.PrettyMIDI(args.training_song_filename).get_piano_roll())
-    # print(df)
     # 0 is the latest observed sample.
     x_offsets = np.sort(np.concatenate((np.arange(-(seq_length_x - 1), 1, 1),)))
-    # print(x_offsets)
-    # Predict the next one hour
     y_offsets = np.sort(np.arange(args.y_start, (seq_length_y + 1), 1))
-    # print(y_offsets)
     # x: (num_samples, input_length, num_nodes, input_dim)
     # y: (num_samples, output_length, num_nodes, output_dim)
     x, y = generate_graph_seq2seq_io_data(
