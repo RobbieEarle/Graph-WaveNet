665e5aefbbf4192ee5b6778f791db4d62a5227c2
rearle <rb504035@dal.ca>
2020-07-01 01:02:23 -0300

    Should hopefully now be able to generate and train on Bach Corale data set

--- generate_song_training_data.py
+++ generate_song_training_data.py
@@ -13,7 +13,7 @@ import util
 
 
 def generate_graph_seq2seq_io_data(
-        midi_data, x_offsets, y_offsets, add_time_in_day=True, add_day_in_week=False, scaler=None
+        pr_data, x_offsets, y_offsets, add_time_in_day=True, add_day_in_week=False, scaler=None
 ):
     """
     Generate samples from
@@ -28,104 +28,68 @@ def generate_graph_seq2seq_io_data(
     # y: (epoch_size, output_length, num_nodes, output_dim)
     """
 
-    num_samples, num_nodes = midi_data.shape
-    midi_data = np.expand_dims(midi_data, axis=-1)
+    num_samples, num_nodes = pr_data.shape
+    midi_data = np.expand_dims(pr_data, axis=-1)
     midi_data = midi_data.astype(int)
-    # x, y = [], []
     min_t = abs(min(x_offsets))
     max_t = abs(num_samples - abs(max(y_offsets)))  # Exclusive
     x = np.zeros((max_t - min_t, len(x_offsets), num_nodes, 1), dtype='uint8')
     y = np.zeros((max_t - min_t, len(y_offsets), num_nodes, 1), dtype='uint8')
-    # print(x.shape)
-    # print(min_t, max_t)
-    # print("asdfs"+234)
     for t in range(min_t, max_t):  # t is the index of the last observation.
 
         x[t - min_t] = midi_data[t + x_offsets, ...]
         y[t - min_t] = midi_data[t + y_offsets, ...]
 
-        # x.append(midi_data[t + x_offsets, ...])
-        # y.append(midi_data[t + y_offsets, ...])
-        # if t == 2000:
-        #     print()
-        #     print(x_offsets)
-        #     print(y_offsets)
-        #     print(t)
-        #     print(t + x_offsets)
-        #     print(t + y_offsets)
-        #     print()
-        #     print(midi_data.shape)
-        #     print(midi_data[t + x_offsets, ...].shape)
-        #     print(midi_data[t + y_offsets, ...].shape)
-    # x = np.stack(x, axis=0)
-    # y = np.stack(y, axis=0)
-    # print(x.shape)
-    # print(x[2500, 0, :, 0])
-    # time.sleep(1)
-    # print("sfda"+234)
     return x, y
 
 
 def generate_train_val_test(args):
     seq_length_x, seq_length_y = args.seq_length_x, args.seq_length_y
-    midi_data = np.transpose(pretty_midi.PrettyMIDI(args.training_song_filename).get_piano_roll(fs=args.fs))
 
-    print(midi_data.shape)
-    print(midi_data[1000])
-    print()
+    if args.dataset == 'maestro':
+        pr_data = np.transpose(pretty_midi.PrettyMIDI(args.training_song_filename).get_piano_roll(fs=args.fs))
 
-    # f = open('data/bach_chorales/chorales_csv/chorales.lisp')
-    # file = f.read()
-    # splitfile = file.split('\n')
-    # print(len(splitfile))
-    # print(splitfile[0])
+    elif args.dataset == 'bch':
 
-    bch_df = pd.read_csv('data/bach_chorales/chorales_csv/bach_choral_set_dataset.csv')
-    print(bch_df.shape)
-    print(bch_df.columns)
-    choral_ids = bch_df.choral_ID.unique()
-    for curr_id in choral_ids:
-        curr_choral_df = bch_df[bch_df['choral_ID'] == curr_id]
+        bch_df = pd.read_csv('data/bach_chorales/chorales_csv/bach_choral_set_dataset.csv')
+        pitches_df = bch_df.iloc[:, 2:14]
+        pitches_df = pitches_df.applymap(lambda x: 1 if x == 'YES' else 0)
+        pitches = pitches_df.to_numpy()
+        velocities_df = bch_df['meter'].apply(lambda x: 10 + int(x / 5 * 100))
+        velocities = velocities_df.to_numpy()
+        velocities = np.expand_dims(velocities, axis=1)
 
-        curr_pitches_df = curr_choral_df.iloc[:, 2:14]
-        curr_pitches_df = curr_pitches_df.applymap(lambda x: 1 if x == 'YES' else 0)
-        # print(curr_pitches_df.shape)
-        curr_pitches = curr_pitches_df.to_numpy()
-        # print(curr_pitches.shape)
+        pr_data = velocities * pitches
 
-        curr_velocities_df = curr_choral_df['meter'].apply(lambda x: 10 + int(x / 5 * 100))
-        curr_velocities = curr_velocities_df.to_numpy()
-        # print(curr_velocities.shape)
-        # print(curr_velocities)
-        # print(curr_pitches.shape)
-        # print(curr_pitches[:10, :])
+    # choral_ids = bch_df.choral_ID.unique()
+    # all_chorals = []
+    # for curr_id in choral_ids:
+    #     curr_choral_df = bch_df[bch_df['choral_ID'] == curr_id]  # Get timesteps from this choral
+    #
+    #     curr_pitches_df = curr_choral_df.iloc[:, 2:14]  # Isolate pitches from other data
+    #     curr_pitches_df = curr_pitches_df.applymap(lambda x: 1 if x == 'YES' else 0)  # Convert YES ->1, NO -> 0
+    #     curr_pitches = curr_pitches_df.to_numpy()  # Convert to Numpy array
+    #
+    #     curr_velocities_df = curr_choral_df['meter'].apply(lambda x: 10 + int(x / 5 * 100))  # Determine velocities
+    #     curr_velocities = curr_velocities_df.to_numpy()  # Convert to Numpy array
+    #     curr_velocities = curr_velocities.reshape((curr_velocities.shape[0], 1))
+    #
+    #     curr_pr = curr_velocities * curr_pitches  # Apply velocities to active pitches
+    #     print(curr_pr.shape)
+    #
+    #     # print(curr_pitches.shape)
+    #     # print(curr_pitches[:10, :])
+    #     #
+    #     time.sleep(1)
+    #     print("asdf" + 1234)
 
-        curr_pitches = curr_velocities.reshape((curr_velocities.shape[0], 1)) * curr_pitches
-        curr_pr = curr_pitches.T
-        left_padding = np.zeros((59, curr_pr.shape[1]))
-        right_padding = np.zeros((37, curr_pr.shape[1]))
-        curr_pr = np.concatenate((left_padding, curr_pr, right_padding), axis=0)
-        curr_midi = util.piano_roll_to_pretty_midi(curr_pr, 1)
-        curr_audio = curr_midi.synthesize(fs=16000)
-        print("Saving " + str(curr_id))
-        np.save('garage/testing_chorales/audio_{}'.format(curr_id), curr_audio)
-
-        # print(curr_pitches.shape)
-        # print(curr_pitches[:10, :])
-        #
-        # time.sleep(1)
-        # print("asdf" + 1234)
-
-    # print(midi_data.shape)
-    time.sleep(1)
-    print("asdf"+1234)
     # 0 is the latest observed sample.
     x_offsets = np.sort(np.concatenate((np.arange(-(seq_length_x - 1), 1, 1),)))
     y_offsets = np.sort(np.arange(args.y_start, (seq_length_y + 1), 1))
     # x: (num_samples, input_length, num_nodes, input_dim)
     # y: (num_samples, output_length, num_nodes, output_dim)
     x, y = generate_graph_seq2seq_io_data(
-        midi_data,
+        pr_data,
         x_offsets=x_offsets,
         y_offsets=y_offsets,
         add_time_in_day=True,
@@ -159,18 +123,19 @@ def generate_train_val_test(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--output_dir", type=str, default="data/song_data", help="Output directory.")
+    parser.add_argument("--output_dir", type=str, default="data/bch_data", help="Output directory.")
     parser.add_argument("--training_song_filename", type=str, default="data/selected_piano/beethoven_tempest.midi", help="Raw traffic readings.",)
-    parser.add_argument("--seq_length_x", type=int, default=500, help="Sequence Length.",)
-    parser.add_argument("--seq_length_y", type=int, default=500, help="Sequence Length.",)
+    parser.add_argument("--seq_length_x", type=int, default=12, help="Sequence Length.",)
+    parser.add_argument("--seq_length_y", type=int, default=12, help="Sequence Length.",)
     parser.add_argument("--y_start", type=int, default=1, help="Y pred start", )
     parser.add_argument("--dow", action='store_true',)
-    parser.add_argument("--fs", type=int, default=20, help="Samples our song every 1/fs of a second",)
+    parser.add_argument("--fs", type=int, default=1, help="Samples our song every 1/fs of a second",)
+    parser.add_argument("--dataset", type=str, default="bch", help="Which dataset to use. Supports bch or maestro" )
 
     args = parser.parse_args()
-    # if os.path.exists(args.output_dir):
-    #     reply = str(input(f'{args.output_dir} exists. Do you want to overwrite it? (y/n)')).lower().strip()
-    #     if reply[0] != 'y': exit
-    # else:
-    #     os.makedirs(args.output_dir)
+    if os.path.exists(args.output_dir):
+        reply = str(input(f'{args.output_dir} exists. Do you want to overwrite it? (y/n)')).lower().strip()
+        if reply[0] != 'y': exit
+    else:
+        os.makedirs(args.output_dir)
     generate_train_val_test(args)
