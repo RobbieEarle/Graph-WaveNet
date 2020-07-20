774d8643e3b2fbb5002acdcf02eb0d2e10d403fc
rearle <rb504035@dal.ca>
2020-07-20 17:38:26 -0300

    Playing around with different approaches, trying to get GWN to work

--- util.py
+++ util.py
@@ -245,12 +245,7 @@ def bob_loss(preds, labels, null_val=np.nan, w=1):
     mask = mask.float()
     mask /= torch.mean((mask))
     mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
-    # loss = torch.abs(preds - labels)
-    # if labels == 1:
-    #     return -log(yHat)
-    # else:
-    #     return -log(1 - yHat)
-    loss = torch.where(labels == 1, -torch.log(preds) * w, -torch.log(1 - preds))
+    loss = torch.where(labels == 1, -torch.log(preds), -torch.log(1 - preds))
     loss = loss * mask
     loss = torch.where(torch.isnan(loss), torch.zeros_like(loss), loss)
 
