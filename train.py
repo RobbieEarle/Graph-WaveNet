005e70762b720d5329e6b983b34fc0b7392fce75
rearle <rb504035@dal.ca>
2020-07-20 23:32:02 -0300

    Playing around with different approaches, trying to get GWN to work

--- train.py
+++ train.py
@@ -63,8 +63,7 @@ def main():
     # print('loading')
 
     device = torch.device(args.device)
-    # adj_mx = util.load_piano_adj(piano_adj, args.adjtype)
-    adj_mx = piano_adj
+    adj_mx = util.load_piano_adj(piano_adj, args.adjtype)
     dataloader = util.load_dataset(args.data, args.batch_size, args.batch_size, args.batch_size)
     supports = [torch.tensor(i).to(device) for i in adj_mx]
 
