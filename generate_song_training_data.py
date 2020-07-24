34eb921b5eb8997b6098ebc1269a96a4aea3adf1
Zhiyuan Zhang <owen.ounce.zhang@gmail.com>
2020-07-24 16:40:54 -0300

    train on more 2

--- generate_song_training_data.py
+++ generate_song_training_data.py
@@ -127,6 +127,7 @@ def generate_train_val_test(args):
     elif args.dataset == 'dual_bounce_diff_period':
         print("Dual Bouncing with different periods")
         pr_data = np.zeros((5665, 12))
+
         for i in range(5665):
             m = i % 7
             n = i % 11
